9ed9d5a6bd840ae6ab9d540a54594dbc091ded01
Samuel Jales <samueljjales@gmail.com>
2025-06-09 20:57:02 -0300

    Organizing everything

--- analisador.py
+++ analisador.py
@@ -1,56 +1,49 @@
-# Working with CSV archives
-import pandas as pd
-import numpy as np
+import pandas as pd #Acessar arquivos .CSV
+import requests # Ler arquivos
+import fitz  # PyMuPDF (Ler arquivos)
+import io # Ler arquivos
+import decimal # Para cálculos mais precisos
 
-# To visualize the graphics
-import seaborn as sns
-import matplotlib.pyplot as plt
+# Arquivo dos dados de dicionários
+from dictionaries import macrozonas, zonas, frentes_opostas, faixa_restriçao, lados
 
-from re import M
-import requests
-import fitz  # PyMuPDF
-import io
-import decimal
+# Funções principais
 
+def somaVetor(vetor):
+    soma = decimal.Decimal(0)
+    for elemento in vetor:
+        soma += decimal.Decimal(elemento)
 
-def analisa_terreno(codigo, largura, altura, area_input, macrozona, zoneamento):
+    return soma
+
+def detlaterais(lados_recebidos, frentes_opostas, lados):
+    lados1 = lados.copy()
+    match len(lados_recebidos):
+        case 1:
+            oposto = frentes_opostas.get(lados_recebidos[0])
+            lados1.remove(oposto)
+            lados1.remove(lados_recebidos[0])
+            return lados1, oposto
+        case 2:
+            lados1.remove(lados_recebidos[0])
+            lados1.remove(lados_recebidos[1])
+            return lados1, None
+        case 3:
+            lados1.remove(lados_recebidos[0])
+            lados1.remove(lados_recebidos[1])
+            lados1.remove(lados_recebidos[2])
+            return lados1, None
+        case _:
+            return [], None
+
+def analisa_terreno(codigo, largura, altura, area_input, macrozona, zoneamento, faixa, pe_esquerdo):
+    bairro_encontrado = "Não encontrado"
+    quadra = "Não encontrada"
+
     bairrosjp_db = pd.read_csv('bairros.csv', delimiter=',')
 
     #Transformando em lista para poder comparar
     bairros = bairrosjp_db['N_BAIRRO'].tolist()
-
-    #Criando dicionários com as macrozonas e zonas, para associar valores
-    macrozonas = {
-        #Macrozonas / IDAPMAX  / IDAPMIN
-        "ma1": [6,1],
-        "ma2": [4,1],
-        "ma3": [2,1],
-        "mbd": [0.5],
-        "mpa": [1]
-    }
-
-    zonas = {
-        #Zonas  /  TO máxima  / TAP mínima  / recuo Frontal  / recuo lateral  /  Recuo Fundo
-        "zh1": [50,10],
-        "zh2": [55,5],
-        "zh3": [50,5],
-        "zh4": [50,15],
-        "zh5": [50,15],
-        "zcs1": [80,5],
-        "zcs2": [70,5],
-        "zcs3": [65,5],
-        "zcs4": [65,5],
-        "zcs5": [50,25],
-        "zcs6": [30,30],
-        "zcs7": [65,10],
-        "zepa2": [40,40],
-        "zepa3": [40, 40],
-        "zi1": [50,10],
-        "zi2": [50,10],
-        "zbd": [10,80],
-        "seav": [40,15],
-    }
-
 
     loc = codigo
     lote = int(loc[5:])
@@ -94,6 +87,9 @@ def analisa_terreno(codigo, largura, altura, area_input, macrozona, zoneamento):
         area_aproveitada = area*IDAP
         area_por_pav = area*(TO/100)
         pavimentos = (area*IDAP)/((TO/100)*area)
+        if (faixa not in (None, "", 0)):
+            valorAlt = decimal.Decimal(faixa_restriçao.get(faixa, [decimal.Decimal(0)])[0])
+            pavimentos = int(valorAlt / decimal.Decimal(pe_esquerdo))
         taxa_de_ocupaçao = (area_por_pav / area)
 
     else:
@@ -101,7 +97,13 @@ def analisa_terreno(codigo, largura, altura, area_input, macrozona, zoneamento):
         area_aproveitada = area*IDAP
         area_por_pav = area*(TO/100)
         pavimentos = (area*IDAP)/((TO/100)*area)
+        if (faixa not in (None, "", 0)):
+            valorAlt = decimal.Decimal(faixa_restriçao.get(faixa, [decimal.Decimal(0)])[0])
+            pavimentos = int(valorAlt / decimal.Decimal(pe_esquerdo))
         taxa_de_ocupaçao = (area_por_pav / area)
+
+    area_com_escadarias = (area_por_pav*decimal.Decimal(0.7)).quantize(decimal.Decimal('0.001'))
+    porcent_area = ((area_com_escadarias/area_por_pav)*100)
 
     return{
         "bairro": bairro_encontrado,
@@ -110,8 +112,65 @@ def analisa_terreno(codigo, largura, altura, area_input, macrozona, zoneamento):
         "area_total": str(area),
         "area_aproveitada": str(area_aproveitada),
         "area_por_pavimento": str(area_por_pav),
-        "area_com_escadarias": str((area_por_pav*decimal.Decimal(0.7)).quantize(decimal.Decimal('0.001'))),
+        "area_com_escadarias": str(area_com_escadarias),
+        "porcent_area": str(porcent_area),
         "num_de_pavimentos": str(pavimentos),
         "taxa_de_ocupaçao": str(taxa_de_ocupaçao*100),
         "link_do_overlay": url
     }
+
+def calcRecuoPavimentos(zona, lados_recebidos, testada_real, profundidade, pavimentos):
+    area_pavs = []
+    num_frentes = len(lados_recebidos)
+
+    #Descobre as laterais e o fundo
+    laterais, fundo = detlaterais(lados_recebidos, frentes_opostas, lados)
+
+    # Realizar o cálculo da área de cada pavimento
+    for i in range(pavimentos):
+        testada_real_for = testada_real
+        profundidade_for = profundidade
+
+        if fundo != None:
+            if i <= 3:
+                if fundo%2 == 0:
+                    testada_real_for = testada_real_for - zonas.get(zona)[i+8]
+                else:
+                    profundidade_for = profundidade_for - zonas.get(zona)[i+8]
+
+            else:
+                if fundo%2 == 0:
+                    testada_real_for = testada_real_for - eval(zonas.get(zona)[12], {}, {"N": i})
+                else:
+                    profundidade_for = profundidade_for - eval(zonas.get(zona)[12], {}, {"N": i})
+
+        # Faz os cálculos dos recuos laterais
+        for k in range(len(laterais)):
+            if i <= 3:
+                if laterais[k]%2 == 0:
+                    testada_real_for = testada_real_for - zonas.get(zona)[i+3]
+                else:
+                    profundidade_for = profundidade_for - zonas.get(zona)[i+3]
+
+            else:
+                if laterais[k]%2 == 0:
+                    testada_real_for = testada_real_for - eval(zonas.get(zona)[7], {}, {"N": i})
+                else:
+                    profundidade_for = profundidade_for - eval(zonas.get(zona)[7], {}, {"N": i})
+
+        # Faz o cálculos de cada pavimento de acordo com o número de frentes
+        for j in range(num_frentes):
+            # Verifica se o lado é par ou ímpar
+            if lados_recebidos[j]%2 == 0:
+                # Subtraindo as frentes
+                testada_real_for = testada_real_for - zonas.get(zona)[2]
+
+            else:
+                profundidade_for = profundidade_for - zonas.get(zona)[2]
+
+        ResArea = testada_real_for * profundidade_for
+        ResArea = round (ResArea, 3)
+        area_pavs.append(ResArea)
+    
+    return area_pavs
+
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-from analisador import analisa_terreno
-from analisador2 import extrair_dados
-from analisador3 import somaVetor
+from analisador import analisa_terreno, somaVetor, calcRecuoPavimentos, detlaterais
+import decimal
 
 app = Flask(__name__)
 
@@ -12,20 +11,16 @@ def home():
 
     if request.method == "POST":
         form = request.form
-        if (int(form["codigo"][5:]) != 0):
-            resultado = extrair_dados(
-                codigo = form["codigo"]
-            )
-
-        else:
-            resultado = analisa_terreno(
-                codigo     = form["codigo"],
-                largura    = form.get("largura", 0) or 0,
-                altura     = form.get("altura", 0) or 0,
-                area_input = form.get("area") or None,
-                macrozona      = form["macro"],
-                zoneamento       = form["zona"]
-            )
+        resultado = analisa_terreno(
+            codigo     = form["codigo"],
+            largura    = form.get("largura", 0) or 0,
+            altura     = form.get("altura", 0) or 0,
+            area_input = form.get("area") or None,
+            macrozona  = form["macro"],
+            zoneamento = form["zona"],
+            faixa      = form["faixa"] or None,
+            pe_esquerdo= form["pe_esquerdo"] or 2.8
+        )      
 
     return render_template("index.html", resultado = resultado)
 
@@ -44,6 +39,48 @@ def atualizar_escadarias():
     nova_area = float(data.get('nova_area_com_escadarias'))  # Recebe o valor como float
     # Faça algo com nova_area (por exemplo, salvar no banco de dados)
     return jsonify({'message': 'Valor atualizado com sucesso!'})
+
+@app.route('/api/lados', methods=['POST'])
+def receber_lados():
+    # Verifica se a requisição contém dados JSON
+    if not request.is_json:
+        # Retorna um erro se o Content-Type não for application/json
+        return jsonify({"erro": "Content-Type deve ser application/json"}), 400
+
+    # Obtém os dados JSON da requisição
+    data = request.get_json()
+
+    # Extrai o array de 'lados' do JSON.
+    # Usa .get() para evitar KeyError se 'lados' não estiver presente.
+    lados_recebidos = data.get('lados', [])
+
+    resultado1 = analisa_terreno(
+            codigo     = data["codigo"],
+            largura    = data.get("largura", 0) or 0,
+            altura     = data.get("altura", 0) or 0,
+            area_input = data.get("area") or None,
+            macrozona  = data["macro"],
+            zoneamento = data["zona"],
+            faixa      = data["faixa"] or None,
+            pe_esquerdo= data["pe_esquerdo"] or 2.8
+        )
+
+    recuo_pavs = None
+
+    if (decimal.Decimal(data.get("largura", 0)) != 0 and decimal.Decimal(data.get("altura", 0)) != 0):
+        recuo_pavs = calcRecuoPavimentos(
+            zona = data["zona"],
+            lados_recebidos = lados_recebidos,
+            testada_real    = data.get("largura", 0) or 0,
+            profundidade    = data.get("altura", 0) or 0,
+            pavimentos      = resultado1.num_pavimentos
+        )
+
+    return jsonify({
+        "mensagem": "Dados dos lados recebidos com sucesso!",
+        "ladosRecebidos": lados_recebidos, # Retorna os lados recebidos de volta
+        "recuopavs": recuo_pavs
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
